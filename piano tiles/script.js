8a8a54e2806c55b4be0d710931a4e7c9abc90494
omjadhav1910 <sonujadhav7020@gmail.com>
2023-11-23 16:01:11 +0530

    om

--- piano tiles/script.js
+++ piano tiles/script.js
@@ -4,7 +4,7 @@ const logo = document.querySelector(".logo");
 const gamename = document.querySelector(".game-name");
 const namebox = document.querySelector(".name-box");
 const game = document.querySelector("#game");
-const sco   = document.getElementById("score");
+const sco = document.getElementById("score");
 const audio = document.getElementById("audio");
 const ply = document.getElementById("play");
 const out = document.getElementById("out");
@@ -14,20 +14,36 @@ const restart = result_box.querySelector(".restart");
 const text = result_box.querySelector(".score_text");
 let a;
 let tos = 2400;
-let bool1 = bool2 = bool3 = bool4 = true;
-var count=1;
+
+var count = 1;
 var score = 0;
 var step = 0;
-var mar=randomMargin(), mar2;
+var mar = randomMargin(),mar2;
 
-function viewResult(){
+// Retrieve the name from local storage
+let playerName = localStorage.getItem("playerName");
+if (playerName) {
+  result_box.querySelector(".complete_text").innerText =
+    "Congratulations, " + playerName + "! You've completed The Game!";
+}
+
+function viewResult() {
+  const playerName = localStorage.getItem("playerName");
+  const completeText = result_box.querySelector(".complete_text");
+  
+  if (playerName) {
+    completeText.innerText = `Congratulations, ${playerName}! You've completed The Game!`;
+  } else {
+    completeText.innerText = "You've completed The Game!";
+  }
+
   game.style.display = "none";
   results.play();
   result_box.classList.add("activeResult");
   text.innerText = "You've scored " + score + " points";
 }
 
-restart.onclick = ()=>{
+restart.onclick = () => {
   start.style.display = "block";
   namebox.style.display = "block";
   gamename.style.display = "block";
@@ -37,9 +53,9 @@ restart.onclick = ()=>{
   result_box.classList.remove("activeResult");
   sco.innerText = 0;
   audio.currentTime = 0;
-}
+};
 
-function startAudio(){
+function startAudio() {
   audio.play();
 }
 audio.addEventListener("ended", () => {
@@ -47,73 +63,76 @@ audio.addEventListener("ended", () => {
   audio.play();
 });
 
-function speed(e){
+function speed(e) {
   a = setInterval(appendDiv, e);
 }
-function reset(){
+function reset() {
   bool1 = bool2 = bool3 = bool4 = true;
 }
-function outs(){
+function outs() {
   audio.pause();
   out.play();
   setTimeout(viewResult, 1000);
 }
 
-function appendDiv(){
+function appendDiv() {
   var ob = document.createElement("div");
 
-  do{mar2 = randomMargin()}
-  while(mar == mar2){mar = mar2}
-
-  ob.style.marginLeft = mar2+"%";
-  setTimeout(moveDown, 100, ob);
-  ob.onclick = () =>{
-    ob.style.background = "rgba(0,0,0,0.2)"
-    updateScore();
+  do {
+    mar2 = randomMargin();
+  } while (mar == mar2) {
+    mar = mar2;
   }
-  if(score >= 70 && score < 150) step = 1;
-  else if(score >= 150 && score < 400) step = 2;
-  else if(score >= 400 && score < 800) step = 3;
-  else if(score >= 800) step = 4;
+
+  ob.style.marginLeft = mar2 + "%";
+  setTimeout(moveDown, 100, ob);
+  ob.onclick = () => {
+    ob.style.background = "rgba(0,0,0,0.2)";
+    updateScore();
+  };
+  if (score >= 50 && score < 150) step = 1;
+  else if (score >= 150 && score < 400) step = 2;
+  else if (score >= 400 && score < 800) step = 3;
+  else if (score >= 800) step = 4;
   document.getElementById("tiles").prepend(ob);
 }
 
-function randomMargin(){return 25*Math.floor(Math.random()*4)}
+function randomMargin() {
+  return 25 * Math.floor(Math.random() * 4);
+}
 
-function moveDown(e){
+function moveDown(e) {
   e.classList.add("move");
-  if(step == 1){
+  if (step == 1) {
     e.classList.add("speedX1");
-    if(bool1 == true){
+    if (bool1 == true) {
       clearInterval(a);
       speed(300);
       reset();
       bool1 = false;
       tos = 1600;
     }
-  } 
-  else if(step == 2){
+  } else if (step == 2) {
     e.classList.add("speedX2");
-    if(bool2 == true){
+    if (bool2 == true) {
       clearInterval(a);
       speed(250);
       reset();
       bool2 = false;
       tos = 1600;
     }
-  }
-  else if(step == 3){
+  } else if (step == 3) {
     e.classList.add("speedX3");
-    if(bool3 == true){
+    if (bool3 == true) {
       clearInterval(a);
       speed(200);
       reset();
       bool3 = false;
       tos = 1200;
     }
-  } else if(step == 4){
+  } else if (step == 4) {
     e.classList.add("speedX4");
-    if(bool4 == true){
+    if (bool4 == true) {
       clearInterval(a);
       speed(160);
       reset();
@@ -121,24 +140,38 @@ function moveDown(e){
       tos = 1000;
     }
   }
-  setTimeout(removeDiv, tos, e)
+  setTimeout(removeDiv, tos, e);
 }
 
-function updateScore(){
+function updateScore() {
   score++;
   sco.innerText = score;
 }
 
-function removeDiv(e){
+let missedTiles = 0;  
+
+function removeDiv(e) {
   var bg = e.style.background;
-  if(bg == ""){
-    // clearInterval(a);
-    outs();
+  if (bg == "") {
+     
+    missedTiles++;
+    if (missedTiles >= 1) {
+      clearInterval(a);
+      outs();
+    }
+  } else {
+    missedTiles = 0;
+    updateScore();
   }
-  e.remove()
+
+  e.remove();
 }
- 
-start.querySelector("button").onclick = ()=>{
+
+start.querySelector("button").onclick = () => {
+  const playerNameInput = document.querySelector(".om");
+  const playerName = playerNameInput.value;
+  localStorage.setItem("playerName", playerName);
+
   ply.play();
   game.style.display = "block";
   start.style.display = "none";
@@ -146,10 +179,8 @@ start.querySelector("button").onclick = ()=>{
   inst.style.display = "none";
   gamename.style.display = "none";
   logo.style.display = "none";
- 
-   
 
   score = 0;
   speed(400);
   setTimeout(startAudio, 1000);
-}
+};
